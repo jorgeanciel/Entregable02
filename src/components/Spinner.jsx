3e14728b25ec7ae71a04595af2382bf861df2471
jorge chirinos <jorgeanciel29@hotmail.com>
2023-03-08 21:00:05 -0500

    finalizado

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/Spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
